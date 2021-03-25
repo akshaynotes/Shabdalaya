c8ba1ada95d470024c363901e7de560b32f60e8a
Akshay <akshayware1207@gmail.com>
2021-03-26 00:42:19 +0530

    🔥 changed code structure, need to review

---
+++ src/Main.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Body from "./Body";
+import Carousal from "./components/Carousal";
+import Header from "./Header";
+
+function Main() {
+  return (
+    <>
+      <Header />
+      <Carousal />
+      <Body />
+    </>
+  );
+}
+
+export default Main;
